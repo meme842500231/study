a0b66938cfaf22049cb059e3fa93f441033f4da9
meme <842500231@qq.com>
2022-05-26 23:55:11 +0800

    美化代码

--- yyl-ajax/server.js
+++ yyl-ajax/server.js
@@ -7,10 +7,12 @@ app.use(express.json())
 app.get('/test_get', (requst, response) => {
     response.send('hello,杨玉玲!董正阳!')
 })
+
 app.post('/test_post', (requst, response) => {
     console.log('有人请求了',request.body)
     response.send('hello,杨玉玲! post')
 })
+
 app.listen(8090, (err) => {
     if (!err) {
         console.log('测试请求成功了,测试如下')
